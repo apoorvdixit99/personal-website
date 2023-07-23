d29db151a26c6a6e01864a1ad195bb397391cdc8
apoorvdixit99 <apoorvdixit619@gmail.com>
2023-07-23 18:10:58 +0530

    Updated contact

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var tabLinks = document.getElementsByClassName("tab-links");
 var tabContents = document.getElementsByClassName("tab-contents");
 var sideMenu = document.getElementById("sidemenu");
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxM-5jkRJgrpSbnz1Nmmp8QcXGuSK90ApqlmazWiFkQ7x-dZfB7MXBX_ZVdabl7x_ln/exec';
-const form = document.forms['submit-to-google-sheet'];
-const msg = document.getElementById("msg");
+var scriptURL = 'https://script.google.com/macros/s/AKfycbxM-5jkRJgrpSbnz1Nmmp8QcXGuSK90ApqlmazWiFkQ7x-dZfB7MXBX_ZVdabl7x_ln/exec';
+var form = document.forms['submit-to-google-sheet'];
+var msg = document.getElementById("msg");
 
 function openTab(tabName) {
     for(tabLink of tabLinks) {
